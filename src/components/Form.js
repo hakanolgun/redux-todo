f7762d132996f44d564296691049f2160f045cb5
hakanolgun <kaptanhakanolgun@gmail.com>
2021-08-25 10:04:26 +0300

    first

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Form() {
+    return (
+        <form>
+        <input
+          className="new-todo"
+          placeHolder="What needs to be done?"
+          autoFocus
+        />
+      </form>
+    )
+}
+
+export default Form
