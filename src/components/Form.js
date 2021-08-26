597bb69fe8e0676104a4a087f2dd1b5f7467bce0
hakanolgun <kaptanhakanolgun@gmail.com>
2021-08-26 17:00:33 +0300

    add new item, delete item, checked uncheked completed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../redux/todos/todosSlice";
+import { nanoid } from "@reduxjs/toolkit"
+
 
 function Form() {
-    return (
-        <form>
-        <input
-          className="new-todo"
-          placeHolder="What needs to be done?"
-          autoFocus
-        />
-      </form>
-    )
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(title === ""){
+      return
+    }
+    dispatch(
+      addTodo({
+        id: nanoid(),
+        title: title,
+        completed: false,
+      })
+    );
+    setTitle("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        className="new-todo"
+        placeHolder="What needs to be done?"
+        autoFocus
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+    </form>
+  );
 }
 
-export default Form
+export default Form;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,31 +1,36 @@
-import React from 'react'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toggle, destroy } from "../redux/todos/todosSlice";
 
 function TodoList() {
-    return (
-        <ul className="todo-list">
-        <li className="completed">
+  const items = useSelector((state) => state.todos.items);
+  const dispatch = useDispatch();
+  const handleDestroy = (id) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(destroy(id));
+    }
+  };
+  return (
+    <ul className="todo-list">
+      {items.map((todo) => (
+        <li className={todo.completed ? "completed" : ""} key={todo.id}>
           <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>Learn JavaScript</label>
-            <button className="destroy"></button>
+            <input
+              className="toggle"
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => dispatch(toggle({ id: todo.id }))}
+            />
+            <label>{todo.title}</label>
+            <button
+              className="destroy"
+              onClick={() => handleDestroy(todo.id)}
+            ></button>
           </div>
         </li>
-        <li>
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>Learn React</label>
-            <button className="destroy"></button>
-          </div>
-        </li>
-        <li>
-          <div className="view">
-            <input className="toggle" type="checkbox" />
-            <label>Have a life!</label>
-            <button className="destroy"></button>
-          </div>
-        </li>
-      </ul>
-    )
+      ))}
+    </ul>
+  );
 }
 
 export default TodoList;
